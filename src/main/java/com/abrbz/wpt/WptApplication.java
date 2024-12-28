bf9965c114b84a9e778386474bfd7c97fb684529
Alexandre Barbez <gyshido@mac.home>
2024-12-28 21:42:25 +0100

    init repo wpt

---
+++ src/main/java/com/abrbz/wpt/WptApplication.java
@@ -0,0 +1,13 @@
+package com.abrbz.wpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abrbz/wpt/WptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abrbz.wpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
